fee62044a42cc526d975990e953b4f50640cd160
ZackCgl <mark@rubyfinance.nl>
2023-06-05 16:49:47 +0200

    chore: add ssr to cart,

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@unocss/nuxt", "@sidebase/nuxt-auth", "@pinia/nuxt"],
-  ssr: true,
   routeRules: {
-    "/": {
+    "/**": {
       isr: true,
+    },
+    "/cart": {
+      ssr: true,
     },
   },
   components: {
