8bb9ee57222726d473ac0b81208f98cdb9b03931
ZackCgl <mark@rubyfinance.nl>
2023-06-05 15:45:48 +0200

    chore: add static to nitro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    static: true,
+  },
   modules: [
     "@unocss/nuxt",
     "@nuxtjs/i18n",
