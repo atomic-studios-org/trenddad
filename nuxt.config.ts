fc38e7a1590adb21e2f3c3f191c91094125a2fc1
ZackCgl <mark@rubyfinance.nl>
2023-06-05 15:56:36 +0200

    chore: rem i18

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    "@unocss/nuxt",
-    "@nuxtjs/i18n",
-    "@sidebase/nuxt-auth",
-    "@pinia/nuxt",
-  ],
+  modules: ["@unocss/nuxt", "@sidebase/nuxt-auth", "@pinia/nuxt"],
   routeRules: {
     "/**": {
       isr: true,
@@ -14,14 +9,6 @@ export default defineNuxtConfig({
   components: {
     dirs: ["~/components"],
     global: true,
-  },
-  i18n: {
-    vueI18n: "./i18n.config.ts", // if you are using custom path, default
-    detectBrowserLanguage: {
-      useCookie: true,
-      cookieKey: "i18n_redirected",
-      redirectOn: "root", // recommended
-    },
   },
   auth: {
     isEnabled: true,
