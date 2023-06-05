05cbc4ae31ebecff996924a9132726307d82e271
ZackCgl <mark@rubyfinance.nl>
2023-06-05 16:58:46 +0200

    chore: rem clientonly

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,10 +7,8 @@ import Downheader from "~/components/headers/downheader.vue";
 <template>
   <div class="min-h-screen font-inter w-screen">
     <div>
-      <ClientOnly>
-        <Upheader />
-        <Downheader />
-      </ClientOnly>
+      <Upheader />
+      <Downheader />
     </div>
     <slot />
   </div>
