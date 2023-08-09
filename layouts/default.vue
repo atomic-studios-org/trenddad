d9c66db2280b0d56448919e2f64de6779c4b7404
Mark Teekens <mark@rubyfinance.nl>
2023-08-09 15:35:51 +0200

    feat: add seo

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,13 @@ import "normalize.css";
 import Upheader from "~/components/headers/upheader.vue";
 import Downheader from "~/components/headers/downheader.vue";
 import Tsxbar from "@/components/tsxbar.vue";
+useHead({
+  title: "Trend Dad, Acquire the  latest trends you're looking for.",
+  meta: [
+    { name: 'description', content: 'Get the latest trends with your trend dad, buy products and stay trending.' }
+  ],
+  
+})
 </script>
 
 <template>
