6226dc895ddb1402021b6f03dc26978c67dfcf8d
Mark Teekens <mark@rubyfinance.nl>
2023-08-12 15:30:32 +0200

    feature: fixed optional chaining tsxbar

--- components/tsxbar.vue
+++ components/tsxbar.vue
@@ -7,8 +7,8 @@ const { data } = useAsyncData("get-data", async () => {
 
 <template>
   <div class="flex py-2 items-baseline justify-center gap-1 bg-black w-screen">
-    <span class="text-white font-inter text-xs">{{data.orders[0].year}}</span>  
-  <img class="h-6 invert" :src="data.orders[0].image" alt="tsxtech" />
+    <span class="text-white font-inter text-xs">{{data?.orders[0].year}}</span>  
+  <img class="h-6 invert" :src="data?.orders[0].image" alt="tsxtech" />
    <span class="text-white font-inter text-xs">Software. All Rights Reserved</span>  
   </div>
 </template>
