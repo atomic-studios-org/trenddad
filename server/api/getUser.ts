78bd691ef6e4a05193ac2b582253ac937c99b9f1
Mark Teekens <mark@rubyfinance.nl>
2023-08-15 11:01:51 +0200

    feature add cached user

--- server/api/getUser.ts
+++ server/api/getUser.ts
@@ -1,14 +1,24 @@
 import { user } from './../../drizzle/schema';
 import db from "../../drizzle/db";
 import {eq, and} from "drizzle-orm"
-const fetchUsers = async ( email: string) => {
+
+const fetchUsers = cachedFunction(async (email: string) => {
   const userData = await db.select().from(user).where(eq(user.email, email))
   return userData;
-};
+},{
+  maxAge: 60 * 60,
+  name: 'userRequest',
+  swr: true,
+  staleMaxAge: 60 * 60,
+  getKey: (email: string) => email
+}) 
+  
+
 
 export default defineEventHandler(async (event) => {
+  setHeader(event, 'Cache-Control', 'public,max-age=86400;'); // 1 day
   const { email } = await readBody(event);
-  const data = await fetchUsers( email);
+  const data = await fetchUsers(email);
 
   return { data: data };
 });
