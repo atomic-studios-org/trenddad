a8230b0b92075277614dab6b3a0501810a85e365
Mark Teekens <mark@rubyfinance.nl>
2023-08-15 16:02:54 +0200

    feat: add sign up

--- pages/sign-up.vue
+++ pages/sign-up.vue
@@ -1,51 +1,80 @@
 <script lang="ts" setup>
-
-
 const { status, data, signIn, signOut } = useAuth();
 
-const email = ref()
-const password = ref()
-const name = ref()
-const isRequested = ref(false)
-const isError = ref()
-const isLoading = ref(false)
+const email = ref();
+const password = ref("");
+const name = ref();
+const isError = ref();
+const isLoading = ref(false);
+const isWrongEmail = ref(false);
+const isRequested = ref(false);
+const confirmPassword = ref("");
+const isWrongPasswordsMatch = ref(false)
+const isWrongPasswordMatch = ref(false)
 
 const handleSignInGoogle = async () => {
   await signIn("google");
 };
 
 const handleSigninCredentials = async () => {
-  isLoading.value = true
-     //create the user in the database with hashed password
-     const {data, error} = await useFetch("/api/confirmEmail", {
-              method: "POST",
-              body: {
-                name: name.value,
-                email: email.value,
-                password: password.value,
-              },
-            });
-            isLoading.value = false
-            isError.value = error.value
-            setTimeout(() => {
-              isError.value = ""
-            }, 3000);
-           console.log(error.value)
-            if(!error.value ){
-              isRequested.value = true
-           setTimeout(() => {
-            isRequested.value = false
-           }, 4000);
-            }
-           
-        
+  const isMatchPasswords = password.value === confirmPassword.value
+  const regexPassword = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/
+  const isMatchPassword = password.value.match(regexPassword)
+  const regexEmail =
+    /^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$/;
+  const isMatchEmail = email.value.match(regexEmail);
+  if (!isMatchEmail) {
+    isWrongEmail.value = true;
+    setTimeout(() => {
+      isWrongEmail.value = false;
+    }, 3000);
+    
+  }
+  else if (!isMatchPassword) {
+    isWrongPasswordMatch.value = true;
+    setTimeout(() => {
+      isWrongPasswordMatch.value = false;
+    }, 10000);
+    
+  }
+  else if (!isMatchPasswords) {
+    isWrongPasswordsMatch.value = true;
+    confirmPassword.value = "";
+    setTimeout(() => {
+      isWrongPasswordsMatch.value = false;
+    }, 3000);
+    
+  }else{
+    isLoading.value = true;
+  //create the user in the database with hashed password
+  const { data, error } = await useFetch("/api/confirmEmail", {
+    method: "POST",
+    body: {
+      name: name.value,
+      email: email.value,
+      password: password.value,
+    },
+  });
+  isLoading.value = false;
+  isError.value = error.value;
+  setTimeout(() => {
+    isError.value = "";
+  }, 3000);
+  console.log(error.value);
+  if (!error.value) {
+    isRequested.value = true;
+    setTimeout(() => {
+      isRequested.value = false;
+    }, 4000);
+  }
+  }
+  
+};
+
+if (data.value?.user?.email) {
+  navigateTo("/account/settings");
 }
 
-  if (data.value?.user?.email) {
-    navigateTo("/account/settings");
-  }
-
-  
 
 </script>
 
@@ -55,7 +84,7 @@ const handleSigninCredentials = async () => {
       class="px-16 py-20 flex flex-col border-groove border-black border-0.5 rounded-sm"
     >
       <span class="font-bold font-melodrama text-xl">Sign up to continue</span>
-     
+
       <div class="mt-4">
         <button
           @click="() => handleSignInGoogle()"
@@ -78,40 +107,61 @@ const handleSigninCredentials = async () => {
           Sign in with Google
         </button>
       </div>
-      <form @submit.prevent="handleSigninCredentials" class="flex flex-col gap-2 mt-2">
+      <form
+        @submit.prevent="handleSigninCredentials"
+        class="flex flex-col gap-2 mt-2"
+      >
         <label>Name:</label>
         <input
-        required
-        v-model="name"
+          required
+          v-model="name"
           placeholder="Your name"
           type="text"
           class="py-1.5 px-4 w-46 rounded-lg hover:border-2 hover:border-groove hover:border-sky-600 focus:border-sky-600"
         />
         <label>Email:</label>
         <input
-        required
-        v-model="email"
+          required
+          v-model="email"
           placeholder="Your email"
           type="text"
           class="py-1.5 px-4 w-46 rounded-lg hover:border-2 hover:border-groove hover:border-sky-600 focus:border-sky-600"
         />
         <label>Password:</label>
         <input
-        required
-        v-model="password"
+          required
+          v-model="password"
           placeholder="Your password"
           type="password"
           class="py-1.5 px-4 w-46 rounded-lg hover:border-2 hover:border-groove hover:border-sky-600 focus:border-sky-600"
         />
-        <span class="w-60 text-sm text-red-500" v-if="isError">User does already exists.</span>
-        <span class="text-sm text-blue-600" v-if="isRequested">Please check your inbox</span>
-        <button
-        :disabled="isLoading"
-        type="submit"
-          class="bg-black text-white disabled:bg-gray-300 disabled:text-gray-400 py-2 px-4 border-none mt-4 cursor-pointer hover:bg-gray-900"
-         
+        <label>Confirm password:</label>
+        <input
+          required
+          v-model="confirmPassword"
+          placeholder="Your password"
+          type="password"
+          class="py-1.5 px-4 w-46 rounded-lg hover:border-2 hover:border-groove hover:border-sky-600 focus:border-sky-600"
+        />
+        <span class="w-60 text-sm text-red-500" v-if="isError"
+          >User does already exists.</span
         >
-        {{isLoading ? "Signing up..": "Sign up"}}
+        <span class="w-60 text-sm text-red-500" v-if="isWrongPasswordMatch"
+          >Password must have minimum eight characters, at least one upper case English letter, one lower case English letter, one number and one special character</span
+        >
+        <span class="w-60 text-sm text-red-500" v-if="isWrongPasswordsMatch"
+          >Passwords do not match.</span
+        >
+        <span class="text-red-500" v-if="isWrongEmail">Email is not correct</span>
+        <span class="text-sm text-blue-600" v-if="isRequested"
+          >Please check your inbox</span
+        >
+        <button
+          :disabled="isLoading"
+          type="submit"
+          class="bg-black text-white disabled:bg-gray-300 disabled:text-gray-400 py-2 px-4 border-none mt-4 cursor-pointer hover:bg-gray-900"
+        >
+          {{ isLoading ? "Signing up.." : "Sign up" }}
         </button>
       </form>
     </div>
