ede96db6db35c41524d8130dbe203a3a1a639557
Mark Teekens <mark@rubyfinance.nl>
2023-10-13 15:41:04 +0200

    feat: add index styling

--- pages/index.vue
+++ pages/index.vue
@@ -18,14 +18,14 @@ const products = data.value?.data;
   <div>
     <div
       @mouseover="hoverImage = true"
-      class="relative lg:h-60 h-40 w-screen bg-black flex items-center justify-center"
+      class="relative lg:h-40 h-40 w-screen bg-black flex items-center justify-center"
     >
       <nuxt-img
         v-if="!hoverImage"
         :height="400"
         :width="400"
         :quality="100"
-        class="lg:h-60 h-40 object-cover md:w-10/12 w-full"
+        class="lg:h-40 h-40 object-cover md:w-10/12 w-full"
         src="/banner.webp"
         alt="dadbanner"
       />
@@ -34,7 +34,7 @@ const products = data.value?.data;
         :height="400"
         :width="400"
         :quality="40"
-        class="lg:h-60 h-40 object-cover md:w-10/12 w-full"
+        class="lg:h-40 h-40 object-cover md:w-10/12 w-full"
         src="/banner.gif"
         alt="dadbanner"
       />
@@ -43,13 +43,45 @@ const products = data.value?.data;
         class="absolute lg:h-60 h-40 z-10 w-full bg-gradient-to-t from-white/30 opacity-100 to-transparent"
       ></div>
     </div>
+    <div class="flex flex-col px-2 md:px-40 py-6">
+      <div class="flex items-end md:gap-0 gap-10 justify-between">
+        <nuxt-img
+          class="md:h-40 md:w-40 h-20 w-20"
+          src="/meme.webp"
+          alt="meme"
+          :height="100"
+          :width="100"
+        />
+        <div>
+          <div class="flex">
+            <h1
+              class="text-black md:text-5xl text-sm text-purple-300 border border-groove p-4 border-gray-300 font-bold drop-shadow-xl shadow-black font-400 font-satoshi"
+            >
+              The number #1 stash store
+            </h1>
+            <nuxt-img src="/gold.webp" alt="gold" :height="43" :width="30" />
+          </div>
+          <span class="text-xs md:text-xl text-black p-1.5"
+            >Hide your valuable items now inside our products and be safe
+            AF.</span
+          >
+        </div>
+        <div>
+          <NuxtLink
+            to="/#products"
+            class="border hidden md:flex border-groove border-purple-500 transition hover:bg-purple-500 py-1 px-2 md:text-xl text-xs no-underline bg-purple-400 cursor-pointer decoration-none text-white"
+            >Shop now</NuxtLink
+          >
+        </div>
+      </div>
+    </div>
     <!-- <div class="flex flex-col items-center">
       <div class="grid md:grid-cols-2 grid-cols-1 mt-0.5 md:gap-6 md:mt-6">
         <collection-block name="home" image="/collectionimages/home.webp" />
         <collection-block name="cars" image="/collectionimages/cars.webp" />
       </div>
     </div> -->
-    <div class="flex flex-col items-center mt-6">
+    <div id="products" class="flex flex-col items-center md:mt-6 mt-0 px-4">
       <div
         class="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6"
       >
--- uno.config.ts
+++ uno.config.ts
@@ -38,6 +38,13 @@ export default defineConfig({
             provider: "fontshare",
           },
         ],
+        satoshi: [
+          {
+            name: "Satoshi",
+
+            provider: "fontshare",
+          },
+        ],
       },
     }),
   ],
