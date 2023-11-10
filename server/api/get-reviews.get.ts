7f0331ed494c16e9fd26a1035423fd9e8fd80c47
markjkaem <mark.teekens@outlook.com>
2023-11-10 19:20:22 +0100

    feat: add reviews to cache

--- server/api/get-reviews.get.ts
+++ server/api/get-reviews.get.ts
@@ -2,12 +2,14 @@ import { eq } from 'drizzle-orm';
 import { reviews } from './../../drizzle/schema';
 import db from "~/drizzle/db"
 
-export default defineEventHandler(async(event) => {
+export default cachedEventHandler(async(event) => {
 
     const response = await getReview()
 
     return response
-})
+}, {
+    swr: true, maxAge: 99999999
+  })
 
 const getReview = async () => {
 const response = await db.select().from(reviews).limit(20)
