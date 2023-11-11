b6c00ab9806211bca399f74c252c21c0f2b0ec7f
markjkaem <mark@rubyfinance.nl>
2023-11-11 19:54:24 +0100

    feat: add analytics

--- app.vue
+++ app.vue
@@ -1,7 +1,12 @@
+<script>
+import { Analytics } from '@vercel/analytics/react';
+</script>
+
 <template>
   <div>
     <NuxtLayout>
       <NuxtPage />
+      <Analytics />
     </NuxtLayout>
   </div>
 </template>
